b529337bf2d33826f7142a6fc67a33d6a91ecb7c
Florin Pop <popflorin1705@yahoo.com>
2017-01-31 11:54:34 +0200

    default blob speed = 5

--- js/blob.js
+++ js/blob.js
@@ -1,7 +1,7 @@
 function Blob() {
     this.pos = createVector(width/2, height/2);
     this.r = 30;
-    this.speed = 20;
+    this.speed = 5;
     
     this.show = function() {
         fill(100, 0, 200);
