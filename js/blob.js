9252e60125dad6e14fb5e975c283f72ebaab03d9
Florin Pop <popflorin1705@yahoo.com>
2017-01-31 11:25:04 +0200

    moving blob with keys

--- js/blob.js
+++ js/blob.js
@@ -1,9 +1,28 @@
 function Blob() {
     this.pos = createVector(width/2, height/2);
-    this.r = 64;
+    this.r = 30;
+    this.speed = 2;
     
     this.show = function() {
-        fill(255);
+        fill(100, 0, 200);
+        
+        this.move();
+        console.log(this.pos);
         ellipse(this.pos.x, this.pos.y, this.r*2, this.r*2);
+    }
+    
+    this.move = function() {
+        if (keyIsDown(LEFT_ARROW))
+            this.pos.x-=this.speed;
+
+        if (keyIsDown(RIGHT_ARROW))
+            this.pos.x+=this.speed;
+
+        if (keyIsDown(UP_ARROW))
+            this.pos.y-=this.speed;
+
+        if (keyIsDown(DOWN_ARROW))
+            this.pos.y+=this.speed;
+            
     }
 }
--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ var blob;
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
     blob = new Blob();
+    console.log("blobl");
 }
 
 function draw() {
