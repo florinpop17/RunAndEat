24c4428cc1eff05a0ff264535c81758d4ac3802c
Florin Pop <popflorin1705@yahoo.com>
2017-01-31 11:50:52 +0200

    multiple food

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
-var blob;
-var food;
+var blob = [];
+var foods = [];
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     blob = new Blob();
-    food = new Food();
+    
+    for(var i=0; i<5; i++){
+        foods.push(new Food());
+    }
 }
 
 function draw() {
     background(0);
+    
+    foods.forEach(food => {
+        food.show();
+    });
+    
     blob.show();
-    food.show();
 }
